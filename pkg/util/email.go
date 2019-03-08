4b5c4fc67f8b512a40e796fd245b103edff10720
Lin <vincentlin@deloitte.com.tw>
2019-03-08 11:35:10 +0800

    1. Fix buttn ref.

--- pkg/util/email.go
+++ pkg/util/email.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"vincent-gin-go/pkg/setting"
 
 	"gopkg.in/gomail.v2"
 )
@@ -11,9 +12,10 @@ func SendActivationEmail(email string, token string) {
 	m.SetHeader("From", "keepexcelsior@gmail")
 	m.SetHeader("To", email)
 	m.SetHeader("Subject", "ACTIVATE YOUR ACCOUNT!")
-	m.SetBody("text/html", "<html><head><title>Activate your account\n</title></head><body><table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\"><tr><td><table cellspacing=\"0\" cellpadding=\"0\"><tr><td style=\"border-radius: 2px;\" bgcolor=\"#ED2939\"><a href=\"https://www.google.com\" target=\"_blank\" style=\"padding: 8px 12px; border: 1px solid #ED2939;border-radius: 2px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;\">Click</a></td></tr></table></td></tr></table></body></html>")
+	address := "http://localhost:8001/activate" + "?email=" + email + "&token=" + token
+	m.SetBody("text/html", "<html><head><title>Activate your account\n</title></head><body><table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\"><tr><td><table cellspacing=\"0\" cellpadding=\"0\"><tr><td style=\"border-radius: 2px;\" bgcolor=\"#ED2939\"><a href="+address+" target=\"_blank\" style=\"padding: 8px 12px; border: 1px solid #ED2939;border-radius: 2px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;\">Click</a></td></tr></table></td></tr></table></body></html>")
 	fmt.Printf("##SEND MAIL: %v", email)
-	d := gomail.NewPlainDialer("smtp.gmail.com", 587, "keepexcelsior@gmail.com", "keep001122")
+	d := gomail.NewPlainDialer("smtp.gmail.com", 587, setting.EmailSetting.Email, setting.EmailSetting.Password)
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
 	}
